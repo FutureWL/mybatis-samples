884ecb1d6a35429e2732e4cc45790df97c2bf8b2
Future. 魏来 <624263934@qq.com>
2022-04-23 20:51:26 +0800

    更新：初始化 mybatis 项目

--- src/main/java/io/github/futurewl/MyBatisApplication2.java
+++ src/main/java/io/github/futurewl/MyBatisApplication2.java
@@ -2,6 +2,8 @@ package io.github.futurewl;
 
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.List;
+
 /**
  * 功能描述：MyBatis 案例记录
  */
@@ -11,9 +13,13 @@ public class MyBatisApplication2 {
         SqlSession sqlSession;
         sqlSession = MyBatisUtil.getSqlSessionFactoryByXml().openSession();
 //        sqlSession = MyBatisUtil.getSqlSessionFactoryByCode().openSession();
-        RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
-        Role role = roleMapper.findRole("角色名");
-        System.out.println("role_name = >" + role.getRoleName());
+//        RoleMapper roleMapper = sqlSession.getMapper(RoleMapper.class);
+//        Role role = roleMapper.findRole("角色名");
+//        System.out.println("role_name = >" + role.getRoleName());
+
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> userList = userMapper.selectUserList();
+        System.out.println("userList = >" + userList.size());
     }
 
 }
---
+++ src/main/java/io/github/futurewl/UserMapper.java
@@ -0,0 +1,9 @@
+package io.github.futurewl;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> selectUserList();
+
+}
